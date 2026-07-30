bfee3fa8a22ef8ed773da01a5eaacf34f6912e8b
Aly Mouir <aly.mounirr@gmail.com>
2026-07-30 22:00:55 +0300

    Stage 1: root and health endpoints

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World! First Server Ever!!!!');
+app.get("/", (req, res) => {
+  res.send("Hello World! First Server Ever!!!!");
+  res.json({
+    name: "ToDo api",
+    version: "1.0",
+    endpoints: ["/tasks"],
+  });
+});
+
+app.get("/health", (req, res) => {
+  res.json({
+    status: "ok",
+  });
 });
 
 app.listen(port, () => {
